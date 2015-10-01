15c327c0e50002382c9bb79a37ea4e430541d46f
Dmitry <dmitry1940@mail.ru>
2015-10-01 16:06:47 +0300

    slider markup

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
+	<div class="main">
+
+		<div class="lube-slider-wrap">
+			<div class="slider-description"> Любe - с любовью к Вам! </div>
+			<div class="lube-slider">
+				<div class="lube-slider-img">
+					<img src="images/lube-img1.jpg" alt="" />
+				</div>
+				<div class="lube-slider-img">
+					<img src="images/lube-img2.jpg" alt="" />
+				</div>
+				<div class="lube-slider-img">
+					<img src="images/lube-img3.jpg" alt="" />
+				</div>
 			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
+		</div>
+
 	</div>

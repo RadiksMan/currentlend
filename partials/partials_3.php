65ba4eb3dfa3fccb45c3ea757b3d973133e7037c
ArtGarm <artem.garmatyk@mail.ru>
2015-10-01 16:28:04 +0300

    try1

---
+++ partials/partials_3.php
@@ -0,0 +1,21 @@
+<div class="block2-wrap">
+    <div class="block2-header"></div>
+    <div class="block2-convert">
+        <div class="block2-item">
+            <div class="image-content"></div>
+            <div class="text-content"></div>
+        </div>
+        <div class="block2-item">
+            <div class="image-content"></div>
+            <div class="text-content"></div>
+        </div>
+        <div class="block2-item">
+            <div class="image-content"></div>
+            <div class="text-content"></div>
+        </div>
+        <div class="block2-item">
+            <div class="image-content"></div>
+            <div class="text-content"></div>
+        </div>
+    </div>
+</div>

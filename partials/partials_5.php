b74e0ffbb3fa911395bec27202f1dfd5cbeb8874
ArtGarm <artem.garmatyk@mail.ru>
2015-10-02 14:33:56 +0300

    image

---
+++ partials/partials_5.php
@@ -0,0 +1,23 @@
+<div class="block5-wrap">
+    <div class="block5 ">
+        <div class="block5-header"> Услуги конференц зала  </div>
+        <div class="block5-convert">
+            <div class="block5-item viv5">
+                <div class="image-content5"> <img src="images/block5-img1.png" alt="" class="img1" />  </div>
+                <div class="text-content5">Вмещаем до 70 людей </div>
+            </div>
+            <div class="block5-item viv5">
+                <div class="image-content5"> <img src="images/block5-img2.png" alt="" class="img1" /> </div>
+                <div class="text-content5">Находимся в центре <br /> города </div>
+            </div>
+            <div class="block5-item viv5">
+                <div class="image-content5"><img src="images/block5-img3.png" alt="" class="img1" /> </div>
+                <div class="text-content5">Услуги кейтеринга <br /> кухня</div>
+            </div>
+            <div class="block5-item viv5">
+                <div class="image-content5"><img src="images/block5-img4.png" alt="" class="img1" /> </div>
+                <div class="text-content5"> Зал оснащен современным  <br /> оборудованием </div>
+            </div>
+        </div>
+    </div>
+</div>

365eafe39db548a91f8bf8beec8e01dbca250c06
ArtGarm <artem.garmatyk@mail.ru>
2015-10-02 09:56:29 +0300

    xdzf

---
+++ js/develop_2.js
@@ -0,0 +1,11 @@
+function showONdisplay () {
+    $('.block3').appear(function() {
+
+    console.log('333');
+}
+
+$(document).ready(function() {
+console.log('333');
+showONdisplay();
+
+});
--- partials/partials_3.php
+++ partials/partials_3.php
@@ -1,20 +1,20 @@
 <div class="block3-wrap">
-    <div class="block3">
+    <div class="block3 ">
         <div class="block3-header">Вы  замечательно проведете свое время, <br /> потому что, только в ЛЮБЕ:</div>
         <div class="block3-convert">
-            <div class="block3-item">
+            <div class="block3-item animated tdZoomIn ">
                 <div class="image-content"> <img src="images/block3-img1.png" alt="" class="img1" />  </div>
                 <div class="text-content">Приветливый <br />  персонал</div>
             </div>
-            <div class="block3-item">
+            <div class="block3-item animated tdZoomIn">
                 <div class="image-content"> <img src="images/block3-img2.png" alt="" class="img1" /> </div>
                 <div class="text-content">На рынке услуг больше <br />  15 лет</div>
             </div>
-            <div class="block3-item">
+            <div class="block3-item animated tdZoomIn">
                 <div class="image-content"><img src="images/block3-img3.png" alt="" class="img1" /> </div>
                 <div class="text-content">Вкуснейшая европейская <br /> кухня</div>
             </div>
-            <div class="block3-item">
+            <div class="block3-item animated tdZoomIn">
                 <div class="image-content"><img src="images/block3-img4.png" alt="" class="img1" /> </div>
                 <div class="text-content">Комфорт и уют в каждом <br /> уголке </div>
             </div>
